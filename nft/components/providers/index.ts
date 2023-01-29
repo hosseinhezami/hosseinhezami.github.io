754780bac398b8bbcb461e2c300c7df53773a510
Hossein Hezami <hossein.hezami@gmail.com>
2023-01-29 15:34:42 +0330

    Update NFT

--- nft/components/providers/index.ts
+++
@@ -1,3 +0,0 @@
-
-
-export { default as Web3Provider } from "./web3";
--- nft/migrations/2_nftMarket_migration.js
+++
@@ -1,5 +0,0 @@
-const NftMarket = artifacts.require("NftMarket");
-
-module.exports = function (deployer) {
-  deployer.deploy(NftMarket);
-};
